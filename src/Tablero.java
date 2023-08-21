52e0b1152efc612e81bcb1431210b38fe5841f02
Sissy <anasofiapatracajara@gmail.com>
2023-08-21 03:18:19 -0500

    - Gol V3 - - En esta vercion se actualizo el elegir los parametros del ancho, largo y velocidad de las generaciones

--- src/Tablero.java
+++ src/Tablero.java
@@ -1,34 +1,88 @@
 import java.util.Scanner;
 
 public class Tablero {
-    private Reglas reglas;
-
-    public Tablero(Reglas reglas) {
-        this.reglas = reglas;
-    }
+    private static Reglas reglas;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Ingrese el tamaño del tablero (Ancho x Alto): ");
-        int ancho = scanner.nextInt();
-        int alto = scanner.nextInt();
+        System.out.println("Eligue el ancho de tu tablero:");
+        System.out.println("1. 10");
+        System.out.println("2. 20");
+        System.out.println("3. 40");
+        System.out.println("4. 80");
+        int opcionAncho = scanner.nextInt();
+
+        int ancho = 0;
+        switch (opcionAncho) {
+            case 1:
+                ancho = 10;
+                break;
+            case 2:
+                ancho = 20;
+                break;
+            case 3:
+                ancho = 40;
+                break;
+            case 4:
+                ancho = 80;
+                break;
+            default:
+                System.out.println("No es un numero valido, por defecto sera: 20");
+                ancho = 20;
+        }
+
+        System.out.println("Ahora eligue la altua de tu tablero:");
+        System.out.println("1. 10");
+        System.out.println("2. 20");
+        System.out.println("3. 40");
+        int opcionAlto = scanner.nextInt();
+
+        int alto = 0;
+        switch (opcionAlto) {
+            case 1:
+                alto = 10;
+                break;
+            case 2:
+                alto = 20;
+                break;
+            case 3:
+                alto = 40;
+                break;
+            default:
+                System.out.println("Numero no valido, por defecto sera: 20");
+                alto = 20;
+        }
 
         boolean[][] generacionActual = new boolean[alto][ancho];
-        boolean[][] siguienteGeneracion;
 
-        // Inicialización del tablero
         for (int i = 0; i < alto; i++) {
             for (int j = 0; j < ancho; j++) {
                 generacionActual[i][j] = Math.random() < 0.5;
             }
         }
 
-        System.out.println("Ingrese el número de generaciones: ");
+        System.out.println("Eligue el numero de generacines: ");
         int numGeneraciones = scanner.nextInt();
 
-        System.out.println("Ingrese el intervalo de tiempo en milisegundos: ");
-        int intervalo = scanner.nextInt();
+        int intervalo = 0;
+        do {
+            System.out.println("ELigue la velocidad a la que quieres que vaya:");
+            System.out.println("1. 250");
+            System.out.println("2. 1000");
+            int opcionIntervalo = scanner.nextInt();
+
+            switch (opcionIntervalo) {
+                case 1:
+                    intervalo = 250;
+                    break;
+                case 2:
+                    intervalo = 1000;
+                    break;
+                default:
+                    System.out.println("Tu numero no es correcto, por facor eligue entre 1 y 2");
+            }
+        } while (intervalo == 0);
 
         for (int generacion = 0; generacion < numGeneraciones || numGeneraciones == 0; generacion++) {
             limpiarConsola();
@@ -41,8 +95,7 @@ public class Tablero {
                 e.printStackTrace();
             }
 
-            siguienteGeneracion = Reglas.aplicarReglas(generacionActual);
-            generacionActual = siguienteGeneracion;
+            generacionActual = Reglas.aplicarReglas(generacionActual);
         }
 
         scanner.close();
@@ -62,3 +115,4 @@ public class Tablero {
         }
     }
 }
+
